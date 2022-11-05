39b37c1a2eb0ae0aa9d9c5e037ef07d4a6cc4a25
Angela Ng <an.ng@mail.utoronto.ca>
2022-11-05 11:18:57 -0400

    Added documentation and specified package for external functions

--- R/scoreVariant.R
+++ R/scoreVariant.R
@@ -1,3 +1,19 @@
+#' Scores a variant of a protein based on weighted average.
+#'
+#' @param eveScores EVE scores for each residue position that has a possible EVE
+#' score.
+#'
+#' @param posWeights The weights to give each residue position when calculating
+#' the weighted average of EVE scores for the protein variant.  The default is
+#' using equal weights for each residue position i.e. 1/(number of residues with
+#' EVE scores).  This is used when an argument is not passed into the posWeights
+#' parameter.
+#'
+#' @return Returns a double with the weighted EVE score for the protein variant.
+#'
+#' @importFrom methods hasArg
+#' @importFrom stats weighted.mean
+
 scoreVariant <- function(eveScores, posWeights = rep(1/length(eveScores), length(eveScores))) {
   # what to do if there are NaN, can replace with 0 and don't need to change the weights
   # or can change the weight so take that weight and redistribute
@@ -6,7 +22,7 @@ scoreVariant <- function(eveScores, posWeights = rep(1/length(eveScores), length
   posWeightsCopy <- posWeights
 
   # check if eveScores and posWeights are the same length
-  if (hasArg(posWeights)) {
+  if (methods::hasArg(posWeights)) {
     if (length(eveScores) != length(posWeights)) {
       warning("The length of the posWeights vector isn't the same as the length of eveScores.
               The default for posWeights will be used instead.")
@@ -23,6 +39,8 @@ scoreVariant <- function(eveScores, posWeights = rep(1/length(eveScores), length
 
   # calculate the weighted mean
 
-  return(weighted.mean(eveScoresCopy, posWeightsCopy))
+  return(stats::weighted.mean(eveScoresCopy, posWeightsCopy))
 
 }
+
+# [END]
