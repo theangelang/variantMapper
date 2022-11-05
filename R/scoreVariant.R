2e4a3e2cac5f84bfa6e95bc46a5697f98735f91c
Angela Ng <an.ng@mail.utoronto.ca>
2022-11-05 10:12:41 -0400

    Changed name of weights parameter

--- R/scoreVariant.R
+++ R/scoreVariant.R
@@ -1,17 +1,16 @@
-scoreVariant <- function(eveScores, weights = rep(1/length(eveScores), length(eveScores))) {
+scoreVariant <- function(eveScores, posWeights = rep(1/length(eveScores), length(eveScores))) {
   # what to do if there are NaN, can replace with 0 and don't need to change the weights
   # or can change the weight so take that weight and redistribute
 
-  # change weights name maybe since its a func too
   eveScoresCopy <- eveScores
-  weightsCopy <- weights
+  posWeightsCopy <- posWeights
 
-  # check if eveScores and weights are the same length
-  if (hasArg(weights)) {
-    if (length(eveScores) != length(weights)) {
-      warning("The length of the weights vector isn't the same as the length of eveScores.
-              The default for weights will be used instead.")
-      weightsCopy <- rep(1/length(eveScores), length(eveScores))
+  # check if eveScores and posWeights are the same length
+  if (hasArg(posWeights)) {
+    if (length(eveScores) != length(posWeights)) {
+      warning("The length of the posWeights vector isn't the same as the length of eveScores.
+              The default for posWeights will be used instead.")
+      posWeightsCopy <- rep(1/length(eveScores), length(eveScores))
     }
   }
 
@@ -24,6 +23,6 @@ scoreVariant <- function(eveScores, weights = rep(1/length(eveScores), length(ev
 
   # calculate the weighted mean
 
-  return(weighted.mean(eveScoresCopy, weightsCopy))
+  return(weighted.mean(eveScoresCopy, posWeightsCopy))
 
 }
