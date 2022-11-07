ca77ceaa3567b2e8d70a2b45ae3afe601e23d82c
Angela Ng <an.ng@mail.utoronto.ca>
2022-11-07 15:36:54 -0500

    Added function to visualize two variants of one gene at once

--- R/visualizeVariant.R
+++ R/visualizeVariant.R
@@ -4,6 +4,7 @@
 
 visualizeVariant <- function(eveInfo) {
   # TODO: add check to make sure has those columns
+  # TODO: handle the NaN values and turn to 0
   p <- ggplot(eveInfo, aes(x=resPos, y=eveScores)) +
     geom_segment( aes(x=resPos, xend=resPos, y=0, yend=eveScores), color="grey") +
     geom_point(aes(color=eveScores), size=4) +
@@ -12,6 +13,26 @@ visualizeVariant <- function(eveInfo) {
       mid = "gray",
       high = "firebrick1",
       midpoint = 0.5) +
+    theme_light() +
+    theme(
+      panel.grid.major.x = element_blank(),
+      panel.border = element_blank(),
+      axis.ticks.x = element_blank()
+    ) +
+    labs(x = "Residue Position", y = "EVE Score", color = "EVE Score")
+  return(p)
+}
+
+#' Function to visualize EVE scores of two variants at once
+#'
+#' @import ggplot2
+
+visualizeVariant2 <- function(eveInfo1, eveInfo2) {
+  # this one compares variants from different samples onto one gene
+  p <- ggplot(eveInfo1, aes(x=resPos, y=eveScores)) +
+    geom_segment( aes(x=resPos, xend=resPos, y=0, yend=eveScores), color="grey") +
+    geom_point(data=eveInfo1, aes(color="Variant 1"), size=4) +
+    geom_point(data=eveInfo2, aes(color="Variant 2"), size=4) +
     theme_light() +
     theme(
       panel.grid.major.x = element_blank(),
