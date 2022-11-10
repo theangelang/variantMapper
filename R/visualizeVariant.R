4c8452be60490427e83613b9f4064c2c22c8fd98
Angela Ng <an.ng@mail.utoronto.ca>
2022-11-09 21:40:07 -0500

    Added ability to filter graphics to exclude EVE scores of 0 and removed EVE scores of NaN

--- R/visualizeVariant.R
+++ R/visualizeVariant.R
@@ -1,7 +1,7 @@
 #' Visualizes EVE score for a protein variant.
 #'
 #' A function that visualizes EVE scores of a variant at residue positions where
-#' EVE scores are possible.
+#' EVE scores are possible.  Function will replace EVE scores of NaN values with 0.
 #'
 #' @param eveInfo Tibble with EVE scores for each residue
 #' position that has a score calculated by EVE, residue position, wildtype amino
@@ -10,6 +10,11 @@
 #'
 #' @param geneName String that is the name of the gene.  If not supplied the
 #' default is X.
+#'
+#' @param aboveZeroOnly Logical value that indicates whether to include EVE scores
+#' of 0.  The default is FALSE so EVE scores of 0 will be included.  Note if
+#' there are NaN values they will be excluded as well since they will be changed
+#' to 0 first.
 #'
 #' @return A lollipop graph showing the EVE score at each residue position.
 #'
@@ -30,7 +35,7 @@
 #' eveScoresProt <- getEveScores(EveData, varDataProt, protein = TRUE)
 #' eveScoresProt
 #'
-#' variantPlotProt <- visualizeVariant(eveScoresProt)
+#' variantPlotProt <- visualizeVariant(eveScoresProt, aboveZeroOnly = TRUE)
 #' variantPlotProt
 #'
 #' # If the data is in genomic form.
@@ -46,11 +51,25 @@
 #'
 #' @import ggplot2
 
-visualizeVariant <- function(eveInfo, geneName = "X") {
+visualizeVariant <- function(eveInfo, geneName = "X", aboveZeroOnly = FALSE) {
   # TODO: add check to make sure has those columns
-  # TODO: Add filtering away 0s and replace NaN with 0
-  p <- ggplot(eveInfo, aes(x=resPos, y=eveScores)) +
-    geom_segment( aes(x=resPos, xend=resPos, y=0, yend=eveScores), color="grey") +
+
+  eveInfoCopy <- eveInfo
+
+  # Replace NaN with 0
+  if(sum(is.nan(eveInfo$eveScores)) != 0) {
+    warning("eveInfo contains NaN values. They will be replaced with 0 values.")
+    eveInfoCopy$eveScores[is.nan(eveInfoCopy$eveScores)] <- 0
+  }
+
+  if (methods::hasArg(aboveZeroOnly)) {
+    if (isTRUE(aboveZeroOnly)) {
+      eveInfoCopy <- dplyr::filter(eveInfoCopy, eveScores != 0)
+      }
+    }
+
+  p <- ggplot(eveInfoCopy, aes(x=resPos, y=eveScores)) +
+    geom_segment(aes(x=resPos, xend=resPos, y=0, yend=eveScores), color="grey") +
     geom_point(aes(color=eveScores), size=4) +
     scale_colour_gradient2(
       low = "steelblue1",
@@ -74,7 +93,7 @@ visualizeVariant <- function(eveInfo, geneName = "X") {
 #' Visualize EVE scores of two variants for one gene.
 #'
 #' A function that visualizes EVE scores for two variants of the same gene
-#' simultaneously.
+#' simultaneously.  Function will replace EVE scores of NaN values with 0
 #'
 #' @param eveInfo1 Tibble for the first variant with EVE scores for each residue
 #' position that has a score calculated by EVE, residue position, wildtype amino
@@ -88,6 +107,11 @@ visualizeVariant <- function(eveInfo, geneName = "X") {
 #'
 #' @param geneName String that is the name of the gene.  If not supplied the
 #' default is X.
+#'
+#' @param aboveZeroOnly Logical value that indicates whether to include EVE scores
+#' of 0.  The default is FALSE so EVE scores of 0 will be included.  Note if
+#' there are NaN values they will be excluded as well since they will be changed
+#' to 0 first.
 #'
 #' @return A lollipop graph showing the EVE score at each residue position for
 #' both variants.
@@ -120,17 +144,41 @@ visualizeVariant <- function(eveInfo, geneName = "X") {
 #' compareVariantsPlot <- visualizeVariant2(eveScoresProt, eveScoresGen, "NRXN1")
 #' compareVariantsPlot
 #'
+#' compareVariantsPlotAboveZero <- visualizeVariant2(eveScoresProt, eveScoresGen, "NRXN1", aboveZeroOnly = TRUE)
+#' compareVariantsPlotAboveZero
+#'
 #' @import ggplot2
 
-visualizeVariant2 <- function(eveInfo1, eveInfo2, geneName = "X") {
+visualizeVariant2 <- function(eveInfo1, eveInfo2, geneName = "X", aboveZeroOnly = FALSE) {
   # this one compares variants from different samples onto one gene
   # TODO: add check to make sure has those columns
-  # TODO: Add filtering away 0s and replace NaN with 0
-  p <- ggplot(eveInfo1, aes(x=resPos, y=eveScores)) +
-    geom_segment(data=eveInfo1, aes(x=resPos, xend=resPos, y=0, yend=eveScores), color="grey") +
-    geom_point(data=eveInfo1, aes(color="Variant 1"), size=4) +
-    geom_segment(data=eveInfo2, aes(x=resPos, xend=resPos, y=0, yend=eveScores), color="grey") +
-    geom_point(data=eveInfo2, aes(color="Variant 2"), size=4) +
+
+  eveInfo1Copy <- eveInfo1
+  eveInfo2Copy <- eveInfo2
+
+  # Replace NaN with 0
+  if(sum(is.nan(eveInfo1$eveScores)) != 0) {
+    warning("eveInfo1 contains NaN values. They will be replaced with 0 values.")
+    eveInfo1Copy$eveScores[is.nan(eveInfo1Copy$eveScores)] <- 0
+  }
+
+  if(sum(is.nan(eveInfo2$eveScores)) != 0) {
+    warning("eveInfo2 contains NaN values. They will be replaced with 0 values.")
+    eveInfo2Copy$eveScores[is.nan(eveInfo2Copy$eveScores)] <- 0
+  }
+
+  if (methods::hasArg(aboveZeroOnly)) {
+    if (isTRUE(aboveZeroOnly)) {
+      eveInfo1Copy <- dplyr::filter(eveInfo1Copy, eveScores != 0)
+      eveInfo2Copy <- dplyr::filter(eveInfo2Copy, eveScores != 0)
+      }
+    }
+
+  p <- ggplot(eveInfo1Copy, aes(x=resPos, y=eveScores)) +
+    geom_segment(data=eveInfo1Copy, aes(x=resPos, xend=resPos, y=0, yend=eveScores), color="grey") +
+    geom_point(data=eveInfo1Copy, aes(color="Variant 1"), size=4) +
+    geom_segment(data=eveInfo2Copy, aes(x=resPos, xend=resPos, y=0, yend=eveScores), color="grey") +
+    geom_point(data=eveInfo2Copy, aes(color="Variant 2"), size=4) +
     theme_light() +
     theme(
       panel.grid.major.x = element_blank(),
