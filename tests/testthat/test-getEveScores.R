85569d8c5d9cb69d5e0d3c8bb5de89ffbd8616cb
Angela Ng <an.ng@mail.utoronto.ca>
2022-11-10 13:09:52 -0500

    Added tests for helper functions

--- tests/testthat/test-getEveScores.R
+++ tests/testthat/test-getEveScores.R
@@ -46,3 +46,70 @@ test_that("Results have expected columns", {
   expect_equal(colnames(getEveScores(eveData, varDataProt, protein = TRUE)), columns)
   expect_equal(colnames(getEveScores(eveData, varDataGen, protein = FALSE)), columns)
 })
+
+test_that("Helper function uniqueWtAa results are of expected length and columns", {
+  EvePath <- system.file("extdata", "NRX1B_HUMAN_SUBSET.vcf", package = "variantMapper")
+  processedEveData <- processEveData(EvePath)
+
+  columns <- c("wtAa", "resPos")
+
+  result <- uniqueWtAaPos(processedEveData)
+
+  expect_equal(nrow(result), 32)
+  expect_equal(ncol(result), 2)
+  expect_equal(colnames(result), columns)
+})
+
+test_that("Helper function findVariantPosition results are of expected length", {
+  EvePath <- system.file("extdata", "NRX1B_HUMAN_SUBSET.vcf", package = "variantMapper")
+  processedEveData <- processEveData(EvePath)
+
+  varDataGenPath <- system.file("extdata", "variant_data_genomic.csv", package = "variantMapper")
+  varDataGen <- processVariantData(varDataGenPath, protein = FALSE)
+
+  coordinates <- dplyr::distinct(processedEveData, POS)
+
+  result <- findVariantPosition(coordinates, varDataGen[[1,"start"]])
+
+  expect_equal(length(result), 2)
+})
+
+test_that("Helper function constructAltSeq results are of expected length and value", {
+  EvePath <- system.file("extdata", "NRX1B_HUMAN_SUBSET.vcf", package = "variantMapper")
+  processedEveData <- processEveData(EvePath)
+
+  varDataGenPath <- system.file("extdata", "variant_data_genomic.csv", package = "variantMapper")
+  varDataGen <- processVariantData(varDataGenPath, protein = FALSE)
+
+  columns <- c("refCodon", "altCodon", "resPos")
+
+  refData <- dplyr::distinct(processedEveData, POS, REF, resPos)
+
+  result1 <- constructAltSeq(refData = refData,
+                             ntPos = 2,
+                             genomicCoord = 50346854,
+                             varNt = "T")
+  result2 <- constructAltSeq(refData = refData,
+                             ntPos = 3,
+                             genomicCoord = 50346851,
+                             varNt = "T")
+  result3 <- constructAltSeq(refData = refData,
+                             ntPos = 1,
+                             genomicCoord = 50346845,
+                             varNt = "G")
+
+  expect_equal(ncol(result1), 3)
+  expect_equal(nrow(result1), 1)
+  expect_equal(colnames(result1), columns)
+  expect_equal(result1[[1, "altCodon"]], "GTC")
+
+  expect_equal(ncol(result2), 3)
+  expect_equal(nrow(result2), 1)
+  expect_equal(colnames(result2), columns)
+  expect_equal(result2[[1, "altCodon"]], "CAT")
+
+  expect_equal(ncol(result3), 3)
+  expect_equal(nrow(result3), 1)
+  expect_equal(colnames(result3), columns)
+  expect_equal(result3[[1, "altCodon"]], "GCA")
+})
--- tests/testthat/test-processEveData.R
+++ tests/testthat/test-processEveData.R
@@ -20,3 +20,17 @@ test_that("Results have expected columns", {
   processedEveData <- processEveData(EvePath)
   expect_equal(colnames(processedEveData), columns)
 })
+
+test_that("Helper function results are of expected length and columns", {
+  EvePath <- system.file("extdata", "NRX1B_HUMAN_SUBSET.vcf", package = "variantMapper")
+  processedEveData <- processEveData(EvePath)
+
+  columns <- c("wtAa", "resPos", "varAa")
+
+  result <- getProtMutInfo(processedEveData$ProtMut)
+
+  expect_equal(nrow(result), 1824)
+  expect_equal(ncol(result), 3)
+  expect_equal(colnames(result), columns)
+})
+
